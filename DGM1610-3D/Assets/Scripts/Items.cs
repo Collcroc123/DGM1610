eb5a53f2a619ef093eba639503a5379a90bba6b2
Collin Gale <Collin.g086@gmail.com>
2020-01-23 17:55:47 -0700

    Item Script

---
+++ DGM1610-3D/Assets/Scripts/Items.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    bool increaseSize;
+    bool healsPlayer;
+    bool savesGame;
+}
+
+public class Health : Items
+{
+    bool healsPlayer = true;
+    bool increaseSize = false;
+    bool savesGame = false;
+}
+
+public class Powerup : Items
+{
+    bool healsPlayer = false;
+    bool increaseSize = true;
+    bool savesGame = false;
+}
+
+public class Checkpoint : Items
+{
+    bool healsPlayer = false;
+    bool increaseSize = true;
+    bool savesGame = true;
+}
