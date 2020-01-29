5e17ceb1fee741251db3174148d55e72c7f6406f
Collin Gale <Collin.g086@gmail.com>
2020-01-29 11:21:45 -0700

    uEvents
    learned more about uEvents

---
+++ DGM1610-3D/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent, mouseUpEvent, mouseHoldEvent, mouseEnterEvent, mouseExitEvent;
+
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseEnterEvent.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        mouseExitEvent.Invoke();
+    }
+
+    private void OnMouseUp()
+    {
+        mouseUpEvent.Invoke();
+    }
+}
