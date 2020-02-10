59f5e73a6b6875efb0e90496f880bff1fd6cec09
Collin Gale <Collin.g086@gmail.com>
2020-02-10 11:26:18 -0700

    Rigidbodies, colliders, physics, oh my!
    More scripts, improved jumping, rigidbody physics!

--- DGM1610-3D/Assets/Scripts/CharacterMovement.cs
+++ DGM1610-3D/Assets/Scripts/CharacterMovement.cs
@@ -1,45 +1,54 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class CharacterMovement : MonoBehaviour
 {
     public CharacterController controller;
-    private Vector3 positionDirection;
-    private float gravity = -0.5f;
-    public float speed = 5f;
-    public float jumpForce = 10f;
+    private Vector3 positionDirection = Vector3.zero;
+    private float gravity = 75f;
+    public float speed = 10f;
+    public float jumpForce = 0.5f;
     private int maxJump = 2;
-    private int currentJump = 1;
-    
+    private int jumpCount = 1;
+
+    //https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
         //Moving
-        positionDirection.x = Input.GetAxis("Vertical")*speed;
-        positionDirection.y += gravity;
-        positionDirection.z = Input.GetAxis("Horizontal")*-speed;
-        controller.Move(positionDirection*Time.deltaTime);
+        positionDirection.x = Input.GetAxis("Horizontal") * speed;
+        positionDirection.z = Input.GetAxis("Vertical") * speed;
+        positionDirection.y -= gravity * Time.deltaTime;
+        controller.Move(positionDirection * Time.deltaTime);
 
         //Jumping
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
-            if(currentJump < maxJump)
+            if (jumpCount < maxJump)
             {
                 positionDirection.y = jumpForce;
-                currentJump++;
+                jumpCount++;
             }
         }
-        if(controller.isGrounded)
+        if (controller.isGrounded)
         {
-            currentJump = 1;
+            jumpCount = 1;
         }
 
         //Running
         if (Input.GetButton("Run"))
         {
-            speed = 10f;
+            speed = 15f;
         }
         else
         {
-            speed = 5f;
-        }
+            speed = 10f;
+        }    
     }
 }
