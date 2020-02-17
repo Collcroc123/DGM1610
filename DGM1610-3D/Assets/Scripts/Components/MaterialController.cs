a22d8e67134c969223d5fee286b4dd3a18ef9f2e
Collin Gale <Collin.g086@gmail.com>
2020-02-16 20:36:49 -0700

    Material Script

---
+++ DGM1610-3D/Assets/Scripts/Components/MaterialController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialController : MonoBehaviour
+{
+    private Material coolMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // it refuses to let me access its .attributes like Metallic, Smoothness, Normal Map and Color
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
