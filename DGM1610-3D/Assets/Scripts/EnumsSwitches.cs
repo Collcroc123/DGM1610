6dbfa2e39561638af52d542d917625dc463dce5e
Collin Gale <Collin.g086@gmail.com>
2020-03-23 11:17:15 -0600

    Switches and Enums
    Useless, but still I'm learning!

---
+++ DGM1610-3D/Assets/Scripts/EnumsSwitches.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnumsSwitches : MonoBehaviour
+{
+    public UnityEvent idleEvent, walkingEvent, runningEvent, jumpingEvent;
+    public UnityEvent startingEvent, playingEvent, pausingEvent, endingEvent;
+
+    public enum GameStates
+    {
+        Starting,
+        Playing,
+        Pausing,
+        Ending
+    }
+
+    public GameStates currentGameState;
+
+    public enum PlayerStates
+    {
+        Idle,
+        Walking,
+        Running,
+        Jumping
+    }
+    
+    public PlayerStates currentPlayerState;
+
+    // Start is called before the first frame update
+    public void ChangeStateToStarting()
+    {
+        currentGameState = GameStates.Starting;
+    }
+
+    public void ChangeStateToPlaying()
+    {
+        currentGameState = GameStates.Playing;
+    }
+
+    public void ChangeStateToPausing()
+    {
+        currentGameState = GameStates.Pausing;
+    }
+
+    public void ChangeStateToEnding()
+    {
+        currentGameState = GameStates.Ending;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentPlayerState)
+        {
+            case PlayerStates.Idle:
+                idleEvent.Invoke();
+                break;
+            case PlayerStates.Walking:
+                walkingEvent.Invoke();
+                break;
+            case PlayerStates.Running:
+                runningEvent.Invoke();
+                break;
+            case PlayerStates.Jumping:
+                jumpingEvent.Invoke();
+                break;
+            default:
+                print("I am none of the above!");
+                break;
+        }
+
+        switch (currentGameState)
+        {
+            case GameStates.Starting:
+                startingEvent.Invoke();
+                break;
+            case GameStates.Playing:
+                playingEvent.Invoke();
+                break;
+            case GameStates.Pausing:
+                pausingEvent.Invoke();
+                break;
+            case GameStates.Ending:
+                endingEvent.Invoke();
+                break;
+            default:
+                print("I am none of the above!");
+                break;
+        }
+    }
+}
---
+++ DGM1610-3D/Assets/Scripts/Weapons.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    public enum weaponsList
+    {
+        pistol,
+        shotgun,
+        sniper,
+        assault_rifle,
+        knife,
+        grenade
+    }
+
+    public weaponsList currentWeapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentWeapon)
+        {
+            case currentWeapon.pistol:
+                break;
+            case currentWeapon.shotgun:
+                break;
+            case currentWeapon.sniper:
+                break;
+            case currentWeapon.assault_rifle:
+                break;
+            case currentWeapon.knife:
+                break;
+            case currentWeapon.grenade:
+                break;
+            default:
+                break;
+        }
+    }
+}
